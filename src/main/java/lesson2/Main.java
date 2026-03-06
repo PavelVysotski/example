e96f5d3489aa7da2f31d71eb7ed002b05490f8d4
PavelVysotski <pavelve83@mail.ru>
2026-03-06 18:45:10 +0300

    lesson2

---
+++ src/main/java/lesson2/Main.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        countNumbers(4, 5);
+    }
+
+    public static void countNumbers(int a, int b) {
+        System.out.println(a + b);
+    }
+}
